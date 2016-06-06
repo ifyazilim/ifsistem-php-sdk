13d2e6bc37f036f686452faeea864dc89aa2c97c
İrfan Evrens <irfanevrens@gmail.com>
2016-06-06 17:12:47 +0300

    manşet modülü

--- src/SistemApi/Model/MansetKategori.php
+++ src/SistemApi/Model/MansetKategori.php
@@ -10,9 +10,37 @@ use SistemApi\Model\Base\Model;
  * @property string kodu @deprecated
  * @property string code
  * @property int site_id
+ * @property array settings
+ * @property int is_active
  * @property Collection languages
  */
 class MansetKategori extends Model
 {
+    public function getSettingCropperAspectRatioWidth()
+    {
+        return isset($this->settings->cropper->aspectRatio->width) ?
+            $this->settings->cropper->aspectRatio->width :
+            850;
+    }
 
+    public function getSettingCropperAspectRatioHeight()
+    {
+        return isset($this->settings->cropper->aspectRatio->height) ?
+            $this->settings->cropper->aspectRatio->height :
+            850;
+    }
+
+    public function getSettingCropperMinCropBoxWidth()
+    {
+        return isset($this->settings->cropper->minCropBox->width) ?
+            $this->settings->cropper->aspectRatio->width :
+            850;
+    }
+
+    public function getSettingCropperMinCropBoxHeight()
+    {
+        return isset($this->settings->cropper->minCropBox->height) ?
+            $this->settings->cropper->aspectRatio->height :
+            850;
+    }
 }
