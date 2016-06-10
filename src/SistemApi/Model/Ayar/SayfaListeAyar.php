c45c0452cfdd47d0bfcb8864b5cdee0ef5f55b22
İrfan Evrens <irfanevrens@gmail.com>
2016-06-10 15:42:20 +0300

    sayfa modülü

--- src/SistemApi/Model/Ayar/SayfaListeAyar.php
+++ src/SistemApi/Model/Ayar/SayfaListeAyar.php
@@ -5,9 +5,21 @@ use SistemApi\Model\Ayar\Base\ListeAyar;
 class SayfaListeAyar extends ListeAyar
 {
     /**
+     * @deprecated
+     *
      * @var int
      */
     private $kategoriId;
+
+    /**
+     * @var int[]
+     */
+    private $categoryIds;
+
+    /**
+     * @var string[]
+     */
+    private $categoryCodes;
 
     /**
      * @return array
@@ -15,11 +27,15 @@ class SayfaListeAyar extends ListeAyar
     public function toArray()
     {
         return array_merge(parent::toArray(), [
-            'kategoriId' => $this->kategoriId
+            'kategoriId' => $this->kategoriId,
+            'category_ids' => $this->categoryIds,
+            'category_codes' => $this->categoryCodes
         ]);
     }
 
     /**
+     * @deprecated use addCategoryId
+     *
      * @param int $kategoriId
      * @return SayfaListeAyar
      */
@@ -30,10 +46,68 @@ class SayfaListeAyar extends ListeAyar
     }
 
     /**
+     * @deprecated use getCategoryIds
+     *
      * @return int
      */
     public function getKategoriId()
     {
         return $this->kategoriId;
+    }
+
+    /**
+     * @param int $categoryId
+     * @return $this
+     */
+    public function addCategoryId($categoryId)
+    {
+        $this->categoryIds[] = $categoryId;
+        return $this;
+    }
+
+    /**
+     * @param \int[] $categoryIds
+     * @return SayfaListeAyar
+     */
+    public function setCategoryIds($categoryIds)
+    {
+        $this->categoryIds = $categoryIds;
+        return $this;
+    }
+
+    /**
+     * @return \int[]
+     */
+    public function getCategoryIds()
+    {
+        return $this->categoryIds;
+    }
+
+    /**
+     * @param string $categoryCode
+     * @return $this
+     */
+    public function addCategoryCode($categoryCode)
+    {
+        $this->categoryCodes[] = $categoryCode;
+        return $this;
+    }
+
+    /**
+     * @param \string[] $categoryCodes
+     * @return SayfaListeAyar
+     */
+    public function setCategoryCodes($categoryCodes)
+    {
+        $this->categoryCodes = $categoryCodes;
+        return $this;
+    }
+
+    /**
+     * @return \string[]
+     */
+    public function getCategoryCodes()
+    {
+        return $this->categoryCodes;
     }
 }
